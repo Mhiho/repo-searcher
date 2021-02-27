fe90af6fcd6cbe30ec159e62e823fe2e5f81d00e
Mhiho <m.pelka7@gmail.com>
2021-02-27 15:02:18 +0100

    pb with deploy on gh-pages test3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Route, BrowserRouter as Router, HashRouter } from "react-router-dom";
 import RepoSearcher from "./components/RepoSearcher";
 import AppDescription from "./components/AppDescription";
 import MathFunction from "./components/MathFunction";
-import PageNotFound from "./components/PageNotFound";
 import { ThemeProvider } from "./store/contextAPI";
 
 function App() {
@@ -13,7 +12,7 @@ function App() {
         <Router>
           <Route path={`/description`} component={AppDescription} />
           <Route path={`/math`} component={MathFunction} />
-          <Route path={`/`} component={RepoSearcher} />
+          <Route exact path={`/`} component={RepoSearcher} />
         </Router>
       </ThemeProvider>
     </HashRouter>
